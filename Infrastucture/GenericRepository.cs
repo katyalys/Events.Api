7af82b78818a2bbd2907df0b864dbcf636bcd90e
katyalys <katyalysykh14@gmail.com>
2023-04-08 23:44:28 +0300

    Add generic repository

---
+++ Infrastucture/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Core.Entity;
+using Core.Interfaces;
+using Infrastucture.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastucture
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+	{
+		private readonly EventsDbContext _context;
+		public GenericRepository(EventsDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task AddAsync(T entity)
+		{
+			await _context.Set<T>().AddAsync(entity);
+		}
+
+		public void Delete(T entity)
+		{
+			_context.Set<T>().Remove(entity);
+		}
+
+		public async Task<T> GetByIdAsync(int id)
+		{
+			return await _context.Set<T>().FindAsync(id);
+		}
+
+		public async Task<IReadOnlyList<T>> ListAllAsync()
+		{
+			return await _context.Set<T>().ToListAsync();
+		}
+
+		public async Task SaveAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
+
+		public void Update(T entity)
+		{
+			_context.Set<T>().Attach(entity);
+			_context.Entry(entity).State = EntityState.Modified;
+		}
+	}
+}
